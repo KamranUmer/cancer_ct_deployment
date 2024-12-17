04acdb06eaaadd1fc24ba474b77fbdab69b629bb
KamranUmer <kamranumer080@gmail.com>
2024-12-17 11:48:00 +0500

    added env

--- chat_bot.py
+++ chat_bot.py
@@ -1,18 +1,19 @@
 import os
-# from dotenv import load_dotenv,find_dotenv
+from dotenv import load_dotenv,find_dotenv
 import streamlit as st
 import google.generativeai as palm
 from langchain_google_genai import ChatGoogleGenerativeAI
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 import os 
 from langchain_groq import ChatGroq
 
-# os.environ["GOOGLE_API_KEY"]= "AIzaSyAx-qltSJhTnuyyJgBMtBaKqz8LIeuXUrs"
+load_dotenv()
 
-GROQ_API = "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"
 
+os.environ["GOOGLE_API_KEY"] = os.getenv("GROQ_API")
+GROQ_API = os.getenv("GROQ_API")
 
 llm = ChatGroq(temperature=0,
                model_name="Llama3-70b-8192",
